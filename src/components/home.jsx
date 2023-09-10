fee55eb278013a5db712b094285770b2bbbea4bb
Suyash Vashishtha <suyashvashishtha@gmail.com>
2023-09-11 02:02:39 +0530

    navigation fix

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,15 +3,17 @@ import './index.css'
 
 export const Home = () => {
 
+    const user = localStorage.getItem("user");
+    const loggedIn = user ? JSON.parse(user).loggedIn : false;
 
     return (
         <div className="page home">
             <h2>👋🏻</h2>
-            <h1>Welcome to my TODO List</h1>
+            <h1>Welcome to my Jira</h1>
             <span>~ made by Jayesh Vashishtha 🧑🏻‍💻</span>
 
             <div className="home__links">
-                <Button  variant="outline-primary">Create Task</Button>
+                <Button href={loggedIn ? '/all' : "/login"} variant="outline-primary">View Tasks</Button>
             </div>
         </div>
     )
